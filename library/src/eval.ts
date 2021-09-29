055580c834bb9ca8393aa894e0a7c50622b02d7f
I`am Ra <78637964+rayyreall@users.noreply.github.com>
2021-09-30 00:04:32 +0800

    Add files via upload

--- library/src/eval.ts
+++ library/src/eval.ts
@@ -1,10 +1,9 @@
 import { HandlingData } from '../typings';
 import { ClientMessage } from '../Base/Scripts/client';
+import { Public,  SettingsPublic } from "../Base/Scripts/main"
 import util from "util";
 import * as ts from "typescript";
-import * as fs from "fs";
 
-let PathConfig: string = "./library/database/config.json";
 
 export var Eval: void = globalThis.Client.on("eval", async (data: HandlingData, Cli: ClientMessage) => {
 	const { args, from, id } = data;
@@ -28,16 +27,14 @@ export var Execute: void = globalThis.Client.on("execute", async (data: Handling
 
 export var Publik: void = globalThis.Client.on("publik", async function (data: HandlingData, Cli: ClientMessage) {
 	const { from, id } = data;
-	let Config: { public: boolean } = JSON.parse(fs.readFileSync(PathConfig).toString())
-	if (Config.public) return Cli.reply(from, "Bot Saat Ini sudah publik", id);
-	Config.public = true;
+	if (Public) return Cli.reply(from, "Bot Saat Ini sudah publik", id);
+	SettingsPublic(true)
 	Cli.reply(from, "Berhasil mengubah status menjadi publik ketik *self* jika kamu ingin mengubah status bot menjadi self", id)
 }, { event: ["publik"], command: ["public", "publik"], withPrefix: false, tag: "owner", isOwner: true })
 
 export var Self: void = globalThis.Client.on("self", async function (data: HandlingData, Cli: ClientMessage) {
 	const { from, id } = data;
-	let Config: { public: boolean } = JSON.parse(fs.readFileSync(PathConfig).toString())
-	if (!Config.public) return Cli.reply(from, "Bot Saat Ini sudah publik", id);
-	Config.public = false;
+	if (!Public) return Cli.reply(from, "Bot Saat Ini sudah Self", id);
+	SettingsPublic(false)
 	Cli.reply(from, "Berhasil mengubah status menjadi self ketik *publik* jika kamu ingin mengubah status bot anda menjadi publik", id)
 }, { event: ["self"], command: ["self"], withPrefix: false, tag: "owner", isOwner: true})
